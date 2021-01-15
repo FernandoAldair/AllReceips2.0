01e304dab875c1a89c19b170920b4420e462d89e
Fernando <fcano@elpuig.xeill.net>
2021-01-15 20:01:39 +0100

    actualizacion

--- app/src/main/java/com/example/allreceips20/LoginFragment.java
+++ app/src/main/java/com/example/allreceips20/LoginFragment.java
@@ -28,58 +28,58 @@ public class LoginFragment extends Fragment {
         return (binding = FragmentLoginBinding.inflate(inflater, container, false)).getRoot();
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        autenticacionViewModel = new ViewModelProvider(requireActivity()).get(AutenticacionViewModel.class);
-        navController = Navigation.findNavController(view);
-
-//        BOTON REGISTRO
-        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_loginFragment_to_registroFragment);
-            }
-        });
-
-//        BOTON INICIAR SESION
-        binding.buttonIniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = binding.Email.getText().toString();
-                String password = binding.Password.getText().toString();
-
-                autenticacionViewModel.iniciarSesion(username, password);
-            }
-        });
-
-//        BOTON INVITADO
-        binding.buttonInvitado.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_loginFragment_to_bottomMenuFragment);
-            }
-        });
-
-
-
-        autenticacionViewModel.estadoDeLaAutenticacion.observe(getViewLifecycleOwner(), new Observer<AutenticacionViewModel.EstadoDeLaAutenticacion>() {
-            @Override
-            public void onChanged(AutenticacionViewModel.EstadoDeLaAutenticacion estadoDeLaAutenticacion) {
-                switch (estadoDeLaAutenticacion){
-                    case AUTENTICADO:
-//                        navController.navigate(R.id.action_iniciarSesionFragment_to_inicioFragment);
-                        break;
-
-                    case AUTENTICACION_INVALIDA:
-                        Toast.makeText(getContext(), "CREDENCIALES NO VALIDAS", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        });
-
-
-
-    }
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+////        autenticacionViewModel = new ViewModelProvider(requireActivity()).get(AutenticacionViewModel.class);
+////        navController = Navigation.findNavController(view);
+////
+//////        BOTON REGISTRO
+////        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                navController.navigate(R.id.action_loginFragment_to_registroFragment);
+////            }
+////        });
+////
+//////        BOTON INICIAR SESION
+////        binding.buttonIniciar.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                String username = binding.Email.getText().toString();
+////                String password = binding.Password.getText().toString();
+////
+////                autenticacionViewModel.iniciarSesion(username, password);
+////            }
+////        });
+////
+//////        BOTON INVITADO
+////        binding.buttonInvitado.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                navController.navigate(R.id.action_loginFragment_to_bottomMenuFragment);
+////            }
+////        });
+////
+////
+////
+////        autenticacionViewModel.estadoDeLaAutenticacion.observe(getViewLifecycleOwner(), new Observer<AutenticacionViewModel.EstadoDeLaAutenticacion>() {
+////            @Override
+////            public void onChanged(AutenticacionViewModel.EstadoDeLaAutenticacion estadoDeLaAutenticacion) {
+////                switch (estadoDeLaAutenticacion){
+////                    case AUTENTICADO:
+//////                        navController.navigate(R.id.action_iniciarSesionFragment_to_inicioFragment);
+////                        break;
+////
+////                    case AUTENTICACION_INVALIDA:
+////                        Toast.makeText(getContext(), "CREDENCIALES NO VALIDAS", Toast.LENGTH_SHORT).show();
+////                        break;
+////                }
+////            }
+////        });
+//
+//
+//
+//    }
 }
